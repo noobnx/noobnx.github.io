03a6b136b877f395ecc469b352d1f5a235bce4f7
Son <info.xuanson@gmail.com>
2023-01-12 12:02:25 +0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+   $('#menu').click(function () {
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+   });
+
+   $(window).on('scroll load', function () {
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+
+      if ($(window).scrollTop() > 0) {
+         $('.btn-on-top').show();
+      } else {
+         $('.btn-on-top').hide();
+      }
+   });
+
+   $('a[href*="#"]').on('click', function (e) {
+      e.preventDefault();
+
+      $('html, body').animate(
+         {
+            scrollTop: $($(this).attr('href')).offset().top,
+         },
+         500,
+         'linear',
+      );
+   });
+
+   $('.arrow-icon').click(function () {
+      $(this).toggleClass('open');
+   });
+});
